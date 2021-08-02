5e04bd89f60510b129514a8b5cbe2079aa0bb170
09Kathy <kdooley@usgs.gov>
2021-08-02 15:51:45 -0500

    host listener works

--- src/app/core/map-options/map-options.component.ts
+++ src/app/core/map-options/map-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { ComponentDisplayService } from 'src/app/shared/services/component-display.service';
@@ -105,6 +105,11 @@ export class MapOptionsComponent implements OnInit {
     }); */
   }
 
+  @HostListener('window:resize')
+  onResize() {
+    this.resizeDivs();
+  }
+
   ngOnInit(): void {
     this.mapForm = new FormGroup({
       northControl: new FormControl(),
@@ -114,111 +119,6 @@ export class MapOptionsComponent implements OnInit {
     });
 
     this.resizeDivs();
-    window.addEventListener('resize', function () {
-      let windowHeight = window.innerHeight;
-      let windowWidth = this.window.innerWidth;
-      let mapPointFilterDiv = document.getElementById('mapOptionsContainer');
-      let mapLayersRadioLabels = document.getElementById('mapLayersID');
-      let mapLayersRadioBtns0 = document.getElementById(
-        'radioContainerResize0'
-      );
-      let radioCheckmarkOuter0 = document.getElementById(
-        'radioCheckmarkOuter0'
-      );
-      let mapLayersRadioBtns1 = document.getElementById(
-        'radioContainerResize1'
-      );
-      let radioCheckmarkOuter1 = document.getElementById(
-        'radioCheckmarkOuter1'
-      );
-      let mapLayersRadioBtns2 = document.getElementById(
-        'radioContainerResize2'
-      );
-      let radioCheckmarkOuter2 = document.getElementById(
-        'radioCheckmarkOuter2'
-      );
-      let mapLayersOptions = document.getElementById('mapLayersOptions');
-      if (windowWidth < 800) {
-        mapLayersOptions.classList.remove('marginLeftFullWidth');
-        mapLayersOptions.classList.add('marginLeftSmallWidth');
-
-        mapPointFilterDiv.classList.remove('marginLeftFullWidth');
-        mapPointFilterDiv.classList.add('marginLeftSmallWidth');
-      }
-      if (windowWidth > 800) {
-        mapLayersOptions.classList.add('marginLeftFullWidth');
-        mapLayersOptions.classList.remove('marginLeftSmallWidth');
-
-        mapPointFilterDiv.classList.add('marginLeftFullWidth');
-        mapPointFilterDiv.classList.remove('marginLeftSmallWidth');
-      }
-      if (windowHeight < 788) {
-        mapPointFilterDiv.classList.remove('optionsBackgroundMapFull');
-        mapPointFilterDiv.classList.add('optionsBackgroundMapResizeH');
-      }
-
-      if (windowHeight > 788) {
-        mapPointFilterDiv.classList.add('optionsBackgroundMapFull');
-        mapPointFilterDiv.classList.remove('optionsBackgroundMapResizeH');
-      }
-      //Edit map layers box when the height shrinks
-      if (windowHeight < 530) {
-        //reduce font size
-        mapLayersRadioLabels.classList.remove('mapLayers');
-        mapLayersRadioLabels.classList.add('mapLayersResize');
-
-        //reduce radio button size
-        mapLayersRadioBtns0.classList.remove('radioContainer');
-        mapLayersRadioBtns0.classList.add('radioContainerResize');
-
-        radioCheckmarkOuter0.classList.remove('radioCheckmark');
-        radioCheckmarkOuter0.classList.add('radioCheckmarkResize');
-
-        mapLayersRadioBtns1.classList.remove('radioContainer');
-        mapLayersRadioBtns1.classList.add('radioContainerResize');
-
-        radioCheckmarkOuter1.classList.remove('radioCheckmark');
-        radioCheckmarkOuter1.classList.add('radioCheckmarkResize');
-
-        mapLayersRadioBtns2.classList.remove('radioContainer');
-        mapLayersRadioBtns2.classList.add('radioContainerResize');
-
-        radioCheckmarkOuter2.classList.remove('radioCheckmark');
-        radioCheckmarkOuter2.classList.add('radioCheckmarkResize');
-
-        //reduce spacing between map layers and nav buttons and decrease background height
-        mapLayersOptions.classList.remove('mapLayerOptionsBackground');
-        mapLayersOptions.classList.add('mapLayerOptionsBackgroundResizeH');
-      }
-      if (windowHeight > 530) {
-        //increase font size
-        mapLayersRadioLabels.classList.add('mapLayers');
-        mapLayersRadioLabels.classList.remove('mapLayersResize');
-
-        //increase radio button size
-        mapLayersRadioBtns0.classList.add('radioContainer');
-        mapLayersRadioBtns0.classList.remove('radioContainerResize');
-
-        radioCheckmarkOuter0.classList.add('radioCheckmark');
-        radioCheckmarkOuter0.classList.remove('radioCheckmarkResize');
-
-        mapLayersRadioBtns1.classList.add('radioContainer');
-        mapLayersRadioBtns1.classList.remove('radioContainerResize');
-
-        radioCheckmarkOuter1.classList.add('radioCheckmark');
-        radioCheckmarkOuter1.classList.remove('radioCheckmarkResize');
-
-        mapLayersRadioBtns2.classList.add('radioContainer');
-        mapLayersRadioBtns2.classList.remove('radioContainerResize');
-
-        radioCheckmarkOuter2.classList.add('radioCheckmark');
-        radioCheckmarkOuter2.classList.remove('radioCheckmarkResize');
-
-        //increase spacing between map layers and nav buttons and increase background height
-        mapLayersOptions.classList.add('mapLayerOptionsBackground');
-        mapLayersOptions.classList.remove('mapLayerOptionsBackgroundResizeH');
-      }
-    });
   }
 
   public displayMapFilters(display: Boolean) {
@@ -243,8 +143,46 @@ export class MapOptionsComponent implements OnInit {
 
   public resizeDivs() {
     let windowHeight = window.innerHeight;
-    console.log('windowHeight', windowHeight);
+    let windowWidth = window.innerWidth;
     let mapPointFilterDiv = document.getElementById('mapOptionsContainer');
+    let mapLayersRadioLabels = document.getElementById('mapLayersID');
+    let mapLayersRadioBtns0 = document.getElementById('radioContainerResize0');
+    let radioCheckmarkOuter0 = document.getElementById('radioCheckmarkOuter0');
+    let mapLayersRadioBtns1 = document.getElementById('radioContainerResize1');
+    let radioCheckmarkOuter1 = document.getElementById('radioCheckmarkOuter1');
+    let mapLayersRadioBtns2 = document.getElementById('radioContainerResize2');
+    let radioCheckmarkOuter2 = document.getElementById('radioCheckmarkOuter2');
+    let mapLayersOptions = document.getElementById('mapLayersOptions');
+    let filterPointsCollapsed = document.getElementById(
+      'filterPointsCollapsed'
+    );
+    let mapLayersCollapsed = document.getElementById('mapLayersCollapsed');
+    if (windowWidth < 800) {
+      mapLayersOptions.classList.remove('marginLeftFullWidth');
+      mapLayersOptions.classList.add('marginLeftSmallWidth');
+
+      mapPointFilterDiv.classList.remove('marginLeftFullWidth');
+      mapPointFilterDiv.classList.add('marginLeftSmallWidth');
+
+      filterPointsCollapsed.classList.remove('marginLeftFullWidth');
+      filterPointsCollapsed.classList.add('marginLeftSmallWidth');
+
+      mapLayersCollapsed.classList.remove('marginLeftFullWidth');
+      mapLayersCollapsed.classList.add('marginLeftSmallWidth');
+    }
+    if (windowWidth > 800) {
+      mapLayersOptions.classList.add('marginLeftFullWidth');
+      mapLayersOptions.classList.remove('marginLeftSmallWidth');
+
+      mapPointFilterDiv.classList.add('marginLeftFullWidth');
+      mapPointFilterDiv.classList.remove('marginLeftSmallWidth');
+
+      filterPointsCollapsed.classList.add('marginLeftFullWidth');
+      filterPointsCollapsed.classList.remove('marginLeftSmallWidth');
+
+      mapLayersCollapsed.classList.add('marginLeftFullWidth');
+      mapLayersCollapsed.classList.remove('marginLeftSmallWidth');
+    }
     if (windowHeight < 788) {
       mapPointFilterDiv.classList.remove('optionsBackgroundMapFull');
       mapPointFilterDiv.classList.add('optionsBackgroundMapResizeH');
@@ -254,8 +192,62 @@ export class MapOptionsComponent implements OnInit {
       mapPointFilterDiv.classList.add('optionsBackgroundMapFull');
       mapPointFilterDiv.classList.remove('optionsBackgroundMapResizeH');
     }
-    if (windowHeight < 150) {
-      console.log('super small');
+    //Edit map layers box when the height shrinks
+    if (windowHeight < 530) {
+      //reduce font size
+      mapLayersRadioLabels.classList.remove('mapLayers');
+      mapLayersRadioLabels.classList.add('mapLayersResize');
+
+      //reduce radio button size
+      mapLayersRadioBtns0.classList.remove('radioContainer');
+      mapLayersRadioBtns0.classList.add('radioContainerResize');
+
+      radioCheckmarkOuter0.classList.remove('radioCheckmark');
+      radioCheckmarkOuter0.classList.add('radioCheckmarkResize');
+
+      mapLayersRadioBtns1.classList.remove('radioContainer');
+      mapLayersRadioBtns1.classList.add('radioContainerResize');
+
+      radioCheckmarkOuter1.classList.remove('radioCheckmark');
+      radioCheckmarkOuter1.classList.add('radioCheckmarkResize');
+
+      mapLayersRadioBtns2.classList.remove('radioContainer');
+      mapLayersRadioBtns2.classList.add('radioContainerResize');
+
+      radioCheckmarkOuter2.classList.remove('radioCheckmark');
+      radioCheckmarkOuter2.classList.add('radioCheckmarkResize');
+
+      //reduce spacing between map layers and nav buttons and decrease background height
+      mapLayersOptions.classList.remove('mapLayerOptionsBackground');
+      mapLayersOptions.classList.add('mapLayerOptionsBackgroundResizeH');
+    }
+    if (windowHeight > 530) {
+      //increase font size
+      mapLayersRadioLabels.classList.add('mapLayers');
+      mapLayersRadioLabels.classList.remove('mapLayersResize');
+
+      //increase radio button size
+      mapLayersRadioBtns0.classList.add('radioContainer');
+      mapLayersRadioBtns0.classList.remove('radioContainerResize');
+
+      radioCheckmarkOuter0.classList.add('radioCheckmark');
+      radioCheckmarkOuter0.classList.remove('radioCheckmarkResize');
+
+      mapLayersRadioBtns1.classList.add('radioContainer');
+      mapLayersRadioBtns1.classList.remove('radioContainerResize');
+
+      radioCheckmarkOuter1.classList.add('radioCheckmark');
+      radioCheckmarkOuter1.classList.remove('radioCheckmarkResize');
+
+      mapLayersRadioBtns2.classList.add('radioContainer');
+      mapLayersRadioBtns2.classList.remove('radioContainerResize');
+
+      radioCheckmarkOuter2.classList.add('radioCheckmark');
+      radioCheckmarkOuter2.classList.remove('radioCheckmarkResize');
+
+      //increase spacing between map layers and nav buttons and increase background height
+      mapLayersOptions.classList.add('mapLayerOptionsBackground');
+      mapLayersOptions.classList.remove('mapLayerOptionsBackgroundResizeH');
     }
   }
 
