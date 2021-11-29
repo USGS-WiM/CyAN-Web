77da02ab03085018512d2ebd8d9e813f36e40c4e
09Kathy <kdooley@usgs.gov>
2021-11-29 11:40:13 -0600

    simple Parameter popup

--- src/app/shared/services/map-layers.service.ts
+++ src/app/shared/services/map-layers.service.ts
@@ -41,6 +41,9 @@ export class MapLayersService {
   public filterWqSampleSubject = new BehaviorSubject<any>(undefined);
   filterWqSample$ = this.filterWqSampleSubject.asObservable();
 
+  public mapQueryResultsSubject = new BehaviorSubject<any>(undefined);
+  mapQueryResults$ = this.mapQueryResultsSubject.asObservable();
+
   //Get map data from service and populate water quality layer
   public filterWqSample(options: {
     meta: {
@@ -75,74 +78,8 @@ export class MapLayersService {
       },
     });
 
-    let mapDataObject = [];
     let mapData;
 
-    this.mapWQSites.on('clusterclick', (cluster) => {
-      setTimeout(() => {
-        //Created this timeout because otherwise it returns the data of the full screen before it zooms to the clicked point
-        //Should probably create some variable (observable?) and an observable at the zoomend thing where if they're both true, then teh following code is triggered
-        //Still need to create a popup
-        let northCoords;
-        let southCoords;
-        let eastCoords;
-        let westCoords;
-        this.componentDisplayService.northBoundsSubject.subscribe(
-          (coord: number) => (northCoords = coord)
-        );
-        this.componentDisplayService.southBoundsSubject.subscribe(
-          (coord: number) => (southCoords = coord)
-        );
-        this.componentDisplayService.eastBoundsSubject.subscribe(
-          (coord: number) => (eastCoords = coord)
-        );
-        this.componentDisplayService.westBoundsSubject.subscribe(
-          (coord: number) => (westCoords = coord)
-        );
-        let matchingPoints = [];
-        let resultValues = [];
-        let pCodes = [];
-        let mCodes = [];
-        let totalSamples = mapData.length;
-        for (let i = 0; i < mapData.length; i++) {
-          let lat = Number(mapData[i].latitude);
-          let lng = Number(mapData[i].longitude);
-          if (
-            lng > westCoords &&
-            lng < eastCoords &&
-            lat < northCoords &&
-            lat > southCoords
-          ) {
-            matchingPoints.push(mapData[i]);
-            pCodes.push(mapData[i].pcode);
-          }
-        }
-        pCodes.sort();
-        let pCodeSummary = [];
-        let tempCode;
-        let count = 0;
-        for (let i = 0; i < pCodes.length; i++) {
-          if (pCodes[i] !== tempCode) {
-            if (i === 0) {
-              console.log('first round');
-              count = 1;
-            }
-            if (i !== 0) {
-              pCodeSummary.push({ pCode: tempCode, count: count });
-              count = 0;
-            }
-          }
-          if (i === pCodes.length - 1) {
-            pCodeSummary.push({ pCode: tempCode, count: count });
-          }
-
-          tempCode = pCodes[i];
-          count += 1;
-        }
-        console.log('matchingPoints', matchingPoints);
-        console.log('pCodeSummary', pCodeSummary);
-      }, 1000);
-    });
     let base = document.getElementById('base');
     base.classList.add('initial-loader');
 
@@ -177,20 +114,14 @@ export class MapLayersService {
             let lat = Number(res[i].latitude);
             let lng = Number(res[i].longitude);
 
-            let tempMapData = new Object();
-            tempMapData['lat'] = res[i].latitude;
-            tempMapData['lng'] = res[i].longitude;
-
-            mapDataObject.push(tempMapData);
-
             L.marker([lat, lng], {
               icon: L.divIcon({
                 className: 'allSiteIcon',
               }),
             }).addTo(this.mapWQSites);
           }
-          console.log('mapDataObject', mapDataObject);
           this.filterWqSampleSubject.next(this.mapWQSites);
+          this.mapQueryResultsSubject.next(mapData);
           base.classList.remove('initial-loader');
           let mapDownloadBtn = document.getElementById('mapDownloadBtn');
           mapDownloadBtn.classList.remove('disabledDataBtn');
