05c8d5e3bb145e9df64773af54d9720a7c286aba
09Kathy <kdooley@usgs.gov>
2022-09-12 15:59:31 -0500

    file upload comments

--- src/app/core/file-upload/file-upload.component.ts
+++ src/app/core/file-upload/file-upload.component.ts
@@ -12,6 +12,8 @@ export class FileUploadComponent {
 
   constructor(private http: HttpClient) {}
 
+  // this code is adapted from: https://stackoverflow.com/questions/27979002/convert-csv-data-into-json-format-using-javascript
+  //get the uploaded data
   uploadedFlags(e) {
     this.file = e.target.files[0];
     let fileReader = new FileReader();
@@ -21,30 +23,21 @@ export class FileUploadComponent {
     };
   }
 
+  //make the csv a json
   csvJSON(csv) {
-    console.log('csv', csv);
     var lines = csv.split('\n');
-
     var result = [];
-
-    // NOTE: If your columns contain commas in their values, you'll need
-    // to deal with those before doing the next step
-    // (you might convert them to &&& or something, then covert them back later)
-    // jsfiddle showing the issue https://jsfiddle.net/
     var headers = lines[0].split(',');
 
     for (var i = 1; i < lines.length; i++) {
       var obj = {};
       var currentline = lines[i].split(',');
-
       for (var j = 0; j < headers.length; j++) {
         obj[headers[j]] = currentline[j];
       }
-
       result.push(obj);
     }
 
-    //return result; //JavaScript object
-    return JSON.stringify(result); //JSON
+    return JSON.stringify(result);
   }
 }
