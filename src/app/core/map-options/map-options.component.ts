93af624876f1a93097d126cef16cc7d42c099f9d
09Kathy <kdooley@usgs.gov>
2023-01-13 09:14:17 -0600

    fix mcodes in map dropdown menu

--- src/app/core/map-options/map-options.component.ts
+++ src/app/core/map-options/map-options.component.ts
@@ -220,6 +220,8 @@ export class MapOptionsComponent implements OnInit {
   //This is called whenever the parameter selection changes
   //It takes the selected parameters and uses them to populate Methods with corresponding methods
   public parameterSelected() {
+    console.log('entering parameterSelected');
+    console.log('this.chipParams', this.chipParams);
     // chips contain the short_name so getting the pcodes
     for (let sn in this.chipParams) {
       let shortname = this.chipParams[sn];
@@ -235,6 +237,8 @@ export class MapOptionsComponent implements OnInit {
       return array.indexOf(value) === index;
     });
 
+    console.log('this.snToPcode', this.snToPcode);
+    console.log('this.mcodeShortName', this.mcodeShortName);
     this.matchingMcodes = [];
     for (let x = 0; x < this.snToPcode.length; x++) {
       let mcodes = [];
@@ -242,8 +246,8 @@ export class MapOptionsComponent implements OnInit {
         if (pcode == this.snToPcode[x]) {
           mcodes.push(this.pcodeToMcode[pcode]);
           for (let i = 0; i < this.mcodeShortName.length; i++) {
-            for (let x = 0; x < mcodes.length; x++) {
-              if (mcodes[x] == this.mcodeShortName[i].mcode) {
+            for (let x = 0; x < mcodes[0].length; x++) {
+              if (mcodes[0][x] == this.mcodeShortName[i].mcode) {
                 this.matchingMcodes.push(this.mcodeShortName[i]);
               }
             }
